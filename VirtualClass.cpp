787c7acb07dfba87661f106ded209fcbafc2a0be
zeshankhalid22 <116242001+zeshankhalid22@users.noreply.github.com>
2023-04-01 14:46:31 +0500

    initial commit

--- VirtualClass.cpp
+++ VirtualClass.cpp
@@ -22,20 +22,11 @@ public:
 
 // Teacher is a derived class of Person, virtually inheriting from Person
 class Teacher: public virtual Person{
-    string Department;
 protected:
     int Salary;
 public:
     // Constructor with initialization list for Name and Salary
     Teacher(string name, int salary) : Person(name),Salary(salary){}
-    
-    // Setter and getter for Department
-    void setDepartment(string department) {
-        Department = department;
-    }
-    string getDepartment(){
-        return Department;
-    }
     // Function to print teacher's salary
     void printTeacher(){
         cout<<"Salary: "<<Salary<<endl;
@@ -44,19 +35,12 @@ public:
 
 // Student is a derived class of Person, virtually inheriting from Person
 class Student: public virtual Person{
-    string rollNum;
 protected:
     char Grade;
 public:
     // Constructor with initialization list for Name and Grade
     Student(string name, char grade):Person(name),Grade(grade){}
     
-    // Setter and getter for rollNum
-    void setRollNum(string roll){
-        rollNum = roll;
-    }
-    string getRollNum(){return rollNum;}
-
     // Function to print student's grade
     void printStudent(){
         cout<<"Grade is: "<<Grade<<endl;
@@ -73,11 +57,8 @@ protected:
 public:
     // Constructor with initialization list for all the inherited members and Email and CourseName
     TA(string name, char grade, int salary, string coursename): Person(name), Teacher(name,salary),
-    Student(name,grade), CourseName(coursename){}
-
-    // Setter and getter for Email
-    void setEmail(string email){Email=email;}
-    string getEmail(){return Email;}
+                                                                Student(name,grade), CourseName(coursename){}
+                                                                
 
     // Function to print TA's information
     void printTA(){
@@ -89,8 +70,7 @@ public:
 };
 
 int main(){
-    // Create an object of TA with name "Zeshan", grade "A", salary 1200, and course name "OOP"
+   
     TA t1("Zeshan",'A',1200,"OOP");
-    t1.setEmail("imzeshankhalid@gmail.com");  // Set TA's email to "imzeshankhalid@gmail.com"
     t1.printTA();   // Print TA's information
 }
